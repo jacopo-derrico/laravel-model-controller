4da14642db15b0033802e03b2348febc292c6e12
jacopo-derrico <derricoj.design@gmail.com>
2024-03-14 17:08:36 +0100

    add bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,6 +5,8 @@
  */
 
 import axios from 'axios';
+import '../scss/app.scss';
+import * as bootstrap from 'bootstrap';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @vite(['resources/js.app.js'])
+
     <title>Movie DB</title>
 </head>
 <body>
-    
+    <main class="container">
+        @foreach ($movies as $movie)
+            <div class="card" style="width: calc(100% / 5);">
+                <div class="card-body">
+                <h5 class="card-title">{{ $movie['title'] }}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        @endforeach
+
+    </main>
 </body>
 </html>
